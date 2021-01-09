ba3ac8a62bd36fc4497325c885a6f00f43d6e281
Miloslav Pavelka <pavelkamiloslav@gmail.com>
2021-01-09 23:59:40 +0100

    Fixing max length for phone number

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 class PIN(models.Model):
-	phone_number = models.CharField("phone number", max_length=5)
+	phone_number = models.CharField("phone number", max_length=16)
 	pin = models.CharField("pin", max_length=5)
 	valid_until = models.DateTimeField("date until pin is valid")
